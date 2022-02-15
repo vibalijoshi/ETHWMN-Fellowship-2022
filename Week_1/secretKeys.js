8e34f101c06eb533cc31780a252e91a5848087ac
vibalijoshi <vibalijoshi@gmail.com>
2022-02-15 16:40:28 +0530

    Added the Secret Keys file

---
+++ Week_1/secretKeys.js
@@ -0,0 +1,10 @@
+const secretKeys = {
+    infuraEndpoint : 'INSERT_YOUR_INFURA_END_POINT',
+    account1Address : 'INSERT_ACCOUNT_ADDRESS',
+    account2Address : 'INSERT_ACCOUNT_ADDRESS',
+    account1PrivateKey : 'INSERT_PRIVATE_KEY'
+    
+
+
+}
+export {secretKeys};
